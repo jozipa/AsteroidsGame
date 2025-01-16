8e9b25a144f72d53c9c840584b0334133cec019e
jozipa <joseadamczyk@gmail.com>
2025-01-16 23:03:27 +0100

    start
    spritesheet json

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+const canvas = document.getElementById('app') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 800;
+console.log(123);
+
+
+
+   
+
+
